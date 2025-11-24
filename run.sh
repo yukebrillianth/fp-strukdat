f4516dac736c8181ec23451d420389f7ef0fd3aa
yukebrillianth <yukebrilliant@gmail.com>
2025-11-24 08:54:58 +0700

    feat: update run script to build and compile SFML dynamically

--- run.sh
+++ run.sh
@@ -1,9 +1,42 @@
 #!/bin/bash
+set -e
 
-clang++ main.cpp -o simulasi \
-  -I/opt/homebrew/include \
-  -L/Users/yukebrillianth/Kuliah/STRUKDAT/fp/sfml-3.0.2/lib \
-  -Wl,-rpath,/Users/yukebrillianth/Kuliah/STRUKDAT/fp/sfml-3.0.2/lib \
-  -lsfml-graphics -lsfml-window -lsfml-system -std=c++17
+if [ ! -d "sfml/install/lib" ]; then
+  echo "-------- Building SFML 3.0.2 --------"
+  
+  cd sfml  
+  mkdir -p build
+  cd build
+  
+  # Configure CMake
+  # -DCMAKE_INSTALL_PREFIX=../install : Tells CMake to put the final files in sfml/install
+  # -DBUILD_SHARED_LIBS=ON            : We want .dylib files (dynamic linking)
+  # -DCMAKE_BUILD_TYPE=Release        : Optimized build
+  cmake .. \
+    -DCMAKE_INSTALL_PREFIX=../install \
+    -DBUILD_SHARED_LIBS=ON \
+    -DCMAKE_BUILD_TYPE=Release
 
-./simulasi
+  cmake --build . --config Release
+  cmake --install . --config Release
+  
+  cd ../..
+  echo "-------- SFML Built Successfully --------"
+fi
+
+echo "-------- Compiling Simulasi --------"
+
+# Explanation of Flags:
+# -I : Look for headers (SFML/Graphics.hpp) in the INSTALL folder
+# -L : Look for library files (libsfml-graphics.dylib) in the INSTALL folder
+# -Wl,-rpath : Tells the resulting executable where to look for .dylib files at RUNTIME
+
+clang++ main.cpp -o run \
+  -I"$(pwd)/sfml/install/include" \
+  -L"$(pwd)/sfml/install/lib" \
+  -Wl,-rpath,"$(pwd)/sfml/install/lib" \
+  -lsfml-graphics -lsfml-window -lsfml-system \
+  -std=c++17
+
+echo "-------- Build Complete --------"
+echo "Run with: ./run"
